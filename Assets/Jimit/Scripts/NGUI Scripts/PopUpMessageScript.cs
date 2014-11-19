e1c63be8fa6aad04deb16f2a322cf7db475169cc
unknown <jbhalani@ETC-JBHALANI.andrew.ad.cmu.edu>
2014-11-19 18:24:18 -0500

    jimit's UI commit

---
+++ Assets/Jimit/Scripts/NGUI Scripts/PopUpMessageScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PopUpMessageScript : MonoBehaviour {
+
+    float timer = 0.0f;
+    bool messageUp = false;
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= 5.0f && !messageUp)
+        {
+            timer = 0.0f;
+            GetComponent<UILabel>().text = " A NEW UPGRADE MESSAGE ";
+            messageUp = true;
+        }
+
+        if (messageUp)
+        {
+            StartCoroutine("hideMessage");
+        }
+	}
+
+    IEnumerator hideMessage()
+    {
+        yield return new WaitForSeconds(1.0f);
+        GetComponent<UILabel>().text = "";
+        messageUp = false;
+    }
+}
---
+++ Assets/Jimit/Scripts/NGUI Scripts/TimerLabelScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerLabelScript : MonoBehaviour {
+
+    float timer = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    timer = (int)Time.timeSinceLevelLoad;
+    GetComponent<UILabel>().text = timer.ToString();
+	}
+}
